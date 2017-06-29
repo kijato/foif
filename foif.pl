0096721cb53a085bd4c0f70d76e75f7b2cc680ec
kijato <kijato@gmail.com>
2017-06-29 08:01:12 +0200

    változások

--- foif.pl
+++ foif.pl
@@ -1,33 +1,40 @@
 #BEGIN{push @INC, '.';}
 use lib '.';
-use foif;
+use Foif;
 #use strict;
-use locale;
+#use locale;
 use Data::Dumper;
+# use Data::Dumper::Perltidy;
+# https://stackoverflow.com/questions/9688096/perl-hash-datadumper
+$Data::Dumper::Indent = 1; # ... makes the output more compact and much more readable when your data structure is several levels deep.
+$Data::Dumper::Sortkeys = 1; # ... makes it easier to scan the output and quickly find the keys you are most interested in.
+$Data::Dumper::Useqq = 1; # If the data structure contains binary data or embedded tabs/newlines, also consider ... which will output a suitable readable representation for that data.
 
+# http://szit.hu/doku.php?id=oktatas:programoz%C3%A1s:perl:per_objektum_orient%C3%A1ltan
 print "¤\n";
-my $job = foif->New($ARGV[0]);
-print "¤\n";
+my $job = Foif->New($ARGV[0]);
+
 #if( $job ) {
-  #while(1) {
+	#while(1) {
+		#print "\nName: ";
+		#$name = <STDIN>;
+		#chop($name);
+		#exit if $name eq "";
+		#$Aref = $job->LookupByName( $name );
+		#if( $Aref ) {
+		#	foreach $number ( @{$Aref} ) { 
+		#		print "$name  $number\n"; 
+		#	}
+		#} else {
+		#	print "$name not found\n";
+		#}
+  print "×\n";
+	print Dumper ( $job->{'fajlnev'}, $job->{'datas'} ) ;
+  print "×\n";
+	# $job->PrintRow($job->{'datas'},'BL');
+	$job->PrintPoints();
 
-      #print "\nName: ";
-      #$name = <STDIN>;
-      #chop($name);
-      #exit if $name eq "";
-      #$Aref = $job->LookupByName( $name );
-      #if( $Aref ) {
-	  #	foreach $number ( @{$Aref} ) { 
-	  #		print "$name  $number\n"; 
-	  #	}
-      #} else {
-	  #	print "$name not found\n";
-      #}
-  print "×\n";
-	  print Dumper $job->{'fajlnev'};
-	  $job->Init();
-	  print Dumper $job->{'metaData'};
-	  print Dumper $job->{'datas'};
-  print "×\n";
   #}
 #}
+print "¤\n";
+#print "\n\nPress <ENTER> to continue!";<STDIN>;
--- foif.pm
+++ foif.pm
@@ -1,101 +1,89 @@
-package foif;
+package Foif;
 
 # use strict;
-use locale; # Ez kell az ABC szerinti helyes rendezéshez.
+# use locale; # Ez kell az ABC szerinti helyes rendezéshez.
+# use encoding "LATIN1";
+# use encoding "LATIN2";
+# use encoding "UTF8";
 # use Data::Dumper;
 
 sub New {
 	my $type = shift;
 	my $self = {
-		'fajlnev'=>shift,
-		'metaData'=>{},
+		'filename'=>shift,
 		'datas'=>[]
 	};
 	bless $self, $type;
+	$self->Init();
 	return $self;
 }
 
 sub Init() {
 
 	my ($self) = @_;
-	my $oneData={};
-	my $antenna="";
-	
-	open FH,$self->{'fajlnev'}||die"$!\n";
+	my $software = "";
+	my $antenna = "";
+	my $oneData = {};
+
+	open FH,$self->{'filename'}||die"$!\n";
 	while(<FH>) {
-		
 		chomp;
-	
-		if ( /^--(FOIF FieldGenius.*)$/ ) {
-			$metaData->{'THE FIRST LINE'}=$1;
+
+		if ( /^--(FieldGenius.*)$/ ) {
+		  $software = '  '.$1;
 		}
-		
-		if ( /^--(Instrument Selected):\s*(.*)$/ or
-			 /^(JB|MO|CS|VA),(.*)$/ )
-		{
-		  $self->{'metaData'}->{$1}=$2;
+
+		if(/^--(Antenna):\s*(.*)$/) {
+			$antenna = $2;
 		}
-	
+
 		if ( /^--GNSS (Profile Tolerance RT):\s*(.*)$/ or
 			 /^--GNSS (Profile Tolerance PP):\s*(.*)$/ or
 			 /^--GNSS (Statistics RT):\s*(.*)$/ or
 			 /^--GNSS (Statistics PP):\s*(.*)$/ or
 			 /^--(PP Time):\s*(.*)$/ or
-			 /^(AH|EP|BL|CV|GS),(.*)$/ )
+			 /^(AH|EP|BL|CV|GS),(.*)$/
+			 or
+			 /^--(Instrument Selected):\s*(.*)$/ or
+			 /^(JB|MO|CS|VA),(.*)$/ )
 		{
 		  $oneData->{$1}=$2;
 		}
-	
-		if(/^--(Antenna):\s*(.*)$/) {
-			$antenna = $2;
-		}
-		
+
 		if ( /^GS,.*$/ ) {
+			$oneData->{'Software'}=$software;
 			$oneData->{'Antenna'}=$antenna;
 			push @{ $self->{'datas'} },$oneData;
 			$oneData={};
 		}
-	
+
 	}
 	close FH;	
 		
 	ParseRecord($self->{'metaData'});
 	ParseRow($self->{'datas'});
-	
-	# return ( $self->{'metaData'}, $self->{'datas'} );
-	return ( $self->{'metaData'} );
-	
+
 }
 
 
-### Functions: ###
-
-sub PrintRow() {
-	my $dataref = shift;
-	my $type = shift;
-	foreach my $r (@{$dataref}) {
-		foreach my $d ( split(/,/,$r->{$type}) ) {
-			$d =~ /(\D{2})(.*)/;
-			print $1.": ".$2."\t";
-		}
-		print "\n";
-	}
-	print "\n";
-}
+##############
+# Functions: #
+##############
 
 sub ParseRecord() {
 	my $r = shift;
 	foreach my $type ( keys %$r ) {
 		my $oneData={};
 		foreach my $d ( split(/,/,$r->{$type}) ) {
-			if ( $d =~ /(\w+)=(\w*)/ ) {
+			#if ( $d =~ /(\w+)=([\w °'."]*)/ ) {
+			if ( $d =~ /(\w+)=(.*)/ ) {
 			$oneData->{$1}=$2;
 			} elsif ( $d =~ /(Not Active)/ ) {
 			$oneData->{$1}=$2;
 			} elsif ( $d =~ /(\D{2})(.*)/ ){
 			$oneData->{$1}=$2;
 			} else {
-			print "\n\nEZMIEZ???\n\n";
+			print "\n\nUnknown data?\n\n";
 			}
 		}
 		$r->{$type}=$oneData;
@@ -109,6 +97,47 @@ sub ParseRow() {
 	}
 }
 
+###########
+# Methods #
+###########
+
+sub PrintPoints() {
+	my $s = shift;
+	foreach my $r (@{$s->{'datas'}}) {
+			print $r->{'JB'}->{'NM'}.";".
+				  $r->{'JB'}->{'DT'}." ".
+				  $r->{'JB'}->{'TM'}.";".
+				  $r->{'Statistics RT'}->{'RefLon'}.";".
+				  $r->{'Statistics RT'}->{'RefLat'}.";".
+				  $r->{'Statistics RT'}->{'RefHgt'}.";".
+				  $r->{'GS'}->{'PN'}.";".
+			      $r->{'GS'}->{'--'}.";".
+			      $r->{'GS'}->{'E '}." [".
+			      $r->{'EP'}->{'RE'}."];".
+			      $r->{'GS'}->{'N '}." [".
+			      $r->{'EP'}->{'RN'}."];".
+			      $r->{'GS'}->{'EL'}." [".
+			      $r->{'EP'}->{'RV'}."];".
+			      "\n";
+		}
+}
+
+sub PrintRow() {
+	print my $dataref = shift;
+	my $type = shift;
+	foreach my $r (@{$dataref}) {
+		foreach my $d ( split(/,/,$r->{$type}) ) {
+			$d =~ /(\D{2})(.*)/;
+			print $1.": ".$2."\t";
+		}
+		print "\n";
+	}
+	print "\n";
+}
+
+1;
+
+__END__
 
 #
 # Telefonkönyv (minat)
@@ -149,5 +178,3 @@ sub LookupByName {
 	my ($self, $name) = @_;
 	return $self{$name};
 }
-
-1;
